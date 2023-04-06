10f84e741795b054422ad6a8e1a4f34e7906d23f
Shane Reynolds <shanejreynolds@outlook.com>
2023-04-06 21:18:05 +0100

    Drum sequencer fully working
    Added new drum samples, images and more.

--- Drum Sequencer/index.js
+++ Drum Sequencer/index.js
@@ -15,8 +15,13 @@ document.getElementById('bpm').addEventListener('change', function (e) {
 
 function loopingFunction(stopFlag) {
     var bpm = 60000 / (document.getElementById('bpm').value);
+    //Audio for loop
     var metronome = new Audio('./audio/metronome.mp3');
     var newBarMetronome = new Audio('./audio/new-bar-metronome.mp3');
+    var kick = new Audio('./audio/kick.mp3');
+    var snare = new Audio('./audio/snare.mp3');
+    var clap = new Audio('./audio/clap.mp3');
+    var cowbell = new Audio('./audio/cowbell.mp3');
     var indexLength = document.getElementsByClassName('time').length;
     for (var i = 0; i < indexLength; i++) {
         (function (i) {
@@ -36,7 +41,23 @@ function loopingFunction(stopFlag) {
                     }
                 }
 
+
+
                 document.getElementsByClassName('time')[i].classList.add('timeActive');
+                //Playing instruments
+                if (document.querySelectorAll('.timeline-kick')[i].classList.contains('kick-active')) {
+                    kick.play();
+                }
+                if (document.querySelectorAll('.timeline-snare')[i].classList.contains('snare-active')) {
+                    snare.play();
+                }
+                if (document.querySelectorAll('.timeline-clap')[i].classList.contains('clap-active')) {
+                    clap.play();
+                }
+                if (document.querySelectorAll('.timeline-cowbell')[i].classList.contains('cowbell-active')) {
+                    cowbell.play();
+                }
+
                 if (i === 0) {
                     document.getElementsByClassName('time')[indexLength - 1].classList.remove('timeActive');
                 }
@@ -87,3 +108,66 @@ document.getElementsByClassName('stopBtn')[0].addEventListener('click', function
 
 });
 
+
+
+function kickPlay() {
+    for (var i = 0; i < document.getElementsByClassName('timeline-kick').length; i++) {
+        (function (i) {
+
+            document.getElementsByClassName('timeline-kick')[i].addEventListener('click', function () {
+                document.getElementsByClassName('timeline-kick')[i].classList.toggle('kick-active');
+            });
+
+
+        }(i));
+    }
+}
+
+function snarePlay() {
+    for (var i = 0; i < document.getElementsByClassName('timeline-snare').length; i++) {
+        (function (i) {
+
+            document.getElementsByClassName('timeline-snare')[i].addEventListener('click', function () {
+                document.getElementsByClassName('timeline-snare')[i].classList.toggle('snare-active');
+            });
+
+
+        }(i));
+    }
+}
+
+function clapPlay() {
+    for (var i = 0; i < document.getElementsByClassName('timeline-clap').length; i++) {
+        (function (i) {
+
+            document.getElementsByClassName('timeline-clap')[i].addEventListener('click', function () {
+                document.getElementsByClassName('timeline-clap')[i].classList.toggle('clap-active');
+            });
+
+
+        }(i));
+    }
+}
+
+function cowbellPlay() {
+    for (var i = 0; i < document.getElementsByClassName('timeline-cowbell').length; i++) {
+        (function (i) {
+
+            document.getElementsByClassName('timeline-cowbell')[i].addEventListener('click', function () {
+                document.getElementsByClassName('timeline-cowbell')[i].classList.toggle('cowbell-active');
+            });
+
+
+        }(i));
+    }
+}
+
+
+
+
+
+//Main Loop
+kickPlay();
+snarePlay();
+clapPlay();
+cowbellPlay();
