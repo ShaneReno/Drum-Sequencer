f6b0ee4b5408308b17cbb1f4d0d2b05a7ad78324
Shane Reynolds <shanejreynolds@outlook.com>
2023-04-13 00:03:28 +0100

    Refactored code and commented
    Clean up and finishing touches.

--- Drum Sequencer/index.js
+++ Drum Sequencer/index.js
@@ -1,4 +1,11 @@
+// JavaScript programmable drum sequencer with effects and parameters.
+//@Shane Reynolds
+//====================================================================
+
+
 var myInterval;
+
+// Error handling for metronome range input
 document.getElementById('bpm').addEventListener('change', function (e) {
     if (document.getElementById('bpm').value == '') {
         e.target.value = 60;
@@ -6,14 +13,15 @@ document.getElementById('bpm').addEventListener('change', function (e) {
     else if (document.getElementById('bpm').value < 60) {
         e.target.value = 60;
     }
-    else if (document.getElementById('bpm').value > 200) {
-        e.target.value = 200;
+    else if (document.getElementById('bpm').value > 160) {
+        e.target.value = 160;
     }
 })
 
 
-
+// The main looping function that performs the iterative logic, sweeping from first beat in the timeline to the last beat, before returning back.
 function loopingFunction() {
+    // The beats per minute is determined by dividing 60,000 (60,000 ms in a minute) into the value of the bpm number input
     var bpm = 60000 / (document.getElementById('bpm').value);
     //Audio for loop
     var metronome = new Audio('./audio/metronome.mp3');
@@ -26,15 +34,11 @@ function loopingFunction() {
 
     for (var i = 0; i < indexLength; i++) {
 
-
         (function (i) {
-
-
             var metronomeCheck = document.getElementById('metronome');
 
-
-
             setTimeout(function () {
+                // Get value of the range input for each instrument, and apply it to the volume of said instrument.
                 for (var j = 0; j <= 3; j++) {
                     var vol = document.getElementsByClassName('form-range')[j].value;
                     document.getElementsByClassName('form-range')[j].style.backgroundSize = vol + '% 100%';
@@ -51,15 +55,12 @@ function loopingFunction() {
                 var cowbellVol = document.getElementsByClassName('cowbellVol')[0].value / 100;
                 cowbell.volume = cowbellVol;
 
-
+                // Perform beat/bar ratio calculation and display.
                 var beatCount = i % 4 + 1;
                 var barCount = Math.ceil((i + 1) / 4);
                 document.getElementsByClassName('barCount')[0].innerHTML = beatCount + '/' + barCount;
 
-
-
-
-
+                // If metronome checkbox is checked, play the new bar metronome tick on the first and fifth bars, or the regular metronome tick.
                 if (metronomeCheck.checked) {
                     if (i % 4 === 0) {
                         newBarMetronome.play();
@@ -70,9 +71,8 @@ function loopingFunction() {
                 }
 
 
-
                 document.getElementsByClassName('time')[i].classList.add('timeActive');
-                //Playing instruments
+                //Playing instruments - if a block on the instruments channel is pressed, pla the note when the sequencer timeline arrives.
                 if (document.querySelectorAll('.timeline-kick')[i].classList.contains('kick-active')) {
                     kick.play();
                 }
@@ -94,7 +94,7 @@ function loopingFunction() {
                 }
 
 
-            }, bpm * i);
+            }, bpm * i); // Correct beats per minute.
 
         }(i));
 
@@ -104,26 +104,28 @@ function loopingFunction() {
 
 
 
-
+// Event listener anonymous function for beginning the looping function.
 document.getElementsByClassName('startBtn')[0].addEventListener('click', function () {
     var bpm = 60000 / (document.getElementById('bpm').value);
 
-    var playBtnAudio = new Audio('./audio/play-btn-audio.mp3');
+    var playBtnAudio = new Audio('./audio/play-btn-audio.mp3'); // Cassette on click.
     playBtnAudio.play();
 
     loopingFunction();
     myInterval = setInterval(loopingFunction, bpm * document.getElementsByClassName('time').length);
+
+    //Error handling to ensure that the user cannot press the start button again once the start button has been pressed. This causes duplicate errors.
     document.getElementsByClassName('startBtn')[0].disabled = true;
     document.getElementsByClassName('stopBtn')[0].disabled = false;
     document.getElementById('bpm').disabled = true;
 
 });
 
-
+// Event listener anonymous function for stopping the looping function.
 document.getElementsByClassName('stopBtn')[0].addEventListener('click', function () {
     clearInterval(myInterval);
     var bpm = 60000 / (document.getElementById('bpm').value);
-    var stopBtnAudio = new Audio('./audio/stop-btn-audio.mp3');
+    var stopBtnAudio = new Audio('./audio/stop-btn-audio.mp3'); // Cassette off click.
     stopBtnAudio.play();
     setInterval(function () {
         document.getElementsByClassName('sequenceBox')[7].classList.remove('timeActive');
@@ -137,6 +139,8 @@ document.getElementsByClassName('stopBtn')[0].addEventListener('click', function
 });
 
 
+
+// Functions for all four drum instruments - once called, they turn an instrument channel block to active (turned on orange)
 
 function kickPlay() {
     for (var i = 0; i < document.getElementsByClassName('timeline-kick').length; i++) {
@@ -191,12 +195,7 @@ function cowbellPlay() {
 }
 
 
-
-
-
-
-
-//Main Loop
+// Calling the event listener functions for drawing the notes on the timeline. 
 kickPlay();
 snarePlay();
 clapPlay();
